765691021962e25635524a9632fe60ec7925cc56
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2025-09-18 18:54:31 -0400

    fix(frontend): restore esm entry imports

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,11 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-var client_1 = require("react-dom/client");
-var App_tsx_1 = require("./App.tsx");
-require("./index.css");
-(0, client_1.createRoot)(document.getElementById('root')).render(<react_1.StrictMode>
-    <App_tsx_1["default"] />
-  </react_1.StrictMode>);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
